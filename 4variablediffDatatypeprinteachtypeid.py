481a064dccde3828b269e6dd21837b0da7046e40
Khushboo05-Singh <143685665+Khushboo05-Singh@users.noreply.github.com>
2023-10-07 02:48:33 +0530

    Add files via upload

---
+++ 4variablediffDatatypeprinteachtypeid.py
@@ -0,0 +1,13 @@
+# Create four variables in a Python script and assign values of different data types to
+# them. Write a Python script to print value, its type and id of each variable
+a=3
+b="Khushi"
+c=3.2
+d=True
+e=3+3j
+
+print(type(a),id(a))
+print(type(b),id(b))
+print(type(c),id(c))
+print(type(d),id(d))
+print(type(e),id(e))
