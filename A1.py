6fc22c92dbe6afe2ce4c3e56115bb4302dbb5c8a
Khushboo05-Singh <143685665+Khushboo05-Singh@users.noreply.github.com>
2023-10-07 02:52:21 +0530

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,6 @@
+set#  Create two Python files A0.py and A1.py. Create a variable in A1.py and assign some
+# value to it. Write a python script to import A1 module in A0 and print value of the
+# variable created in A0.py
+a=5
+b=6
+print("Sum of the numbers is: ",a+b)
